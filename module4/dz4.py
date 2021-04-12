195a4b5b97d9302acb2034907d5a41319be59aaa
ViktoriiaBond <vika.bont1993@gmail.com>
2021-04-13 00:25:18 +0300

    update dz4

--- module4/dz4.py
+++ module4/dz4.py
@@ -1,16 +1,35 @@
 from pathlib import Path
 
 p = Path('/Users/Noname/Downloads')
+music = list()
+photo = list()
+films = list() 
+docs = list() 
+archive = list() 
+unidentified = list() 
 for i in p.iterdir():
     if i in p.glob('*.mp3') or i in p.glob('*.ogg') or i in p.glob('*.waw') or i in p.glob('*.amr'):
-        music = i
+        music.append(i)
+        
     elif i in p.glob('*.jpeg') or i in p.glob('*.png') or i in p.glob('*.jpn') or i in p.glob('*.svg'):
-        photo = i
+        photo.append(i)
+       
     elif i in p.glob('*.avi') or i in p.glob('*.mp4') or i in p.glob('*.mov') or i in p.glob('*.mkv'):
-        films = i 
-    elif i in p.glob('*.docx') or i in p.glob('*.txt') or i in p.glob('*.pdf') or i in p.glob('*.xlsx'):
-        films = i            
-      #DOC', 'DOCX', 'TXT', 'PDF', 'XLSX', 'PPTX
-    #if p.glob('*.mp3'):  
-     #   print(txt_file)        
+        films.append(i)
+    elif i in p.glob('*.docx') or i in p.glob('*.txt') or i in p.glob('*.pdf') or i in p.glob('*.xlsx') or i in p.glob('*.pptx'):
+        docs.append(i)
+       
+    elif i in p.glob('*.zip') or i in p.glob('*.gz') or i in p.glob('*.tar'):
+        archive.append(i)
+                
+    else:
+        unidentified.append(i)
+        
+
+print(music)
+print(photo)
+print(films)
+print(docs)
+print(archive)
+print(unidentified)
        
