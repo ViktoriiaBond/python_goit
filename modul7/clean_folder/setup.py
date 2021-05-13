f3ef3c5ef47ba9a1f1aa04878e828b808e28d8a1
ViktoriiaBond <vika.bont1993@gmail.com>
2021-05-13 17:38:41 +0300

    path

--- modul7/clean_folder/setup.py
+++ modul7/clean_folder/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='clean_folder',
       version='1',
       description='Very useful code',
-      url='https://github.com/ViktoriiaBond/python_goit/blob/main/modul6/dz6.py',
+      url='https://github.com/ViktoriiaBond/python_goit/tree/main/modul7',
       author='Vika',
       author_email='vika.bont1993@gmail.com',
       license='MIT',
